e16a855ae58751a1ea9339b6f72f537375d55922
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-04-18 22:51:34 +0200

    Remove many template action for message
    Now that template can be completely overriden no need of many actions.

--- templates/message.php
+++ templates/message.php
@@ -3,31 +3,15 @@
     <?php do_action('cookie-policy.before-message') ?>
 
     <div class="cookie-policy-msg">
-
-        <?php do_action('cookie-policy.before-message-table') ?>
-
         <table border="0" width="100%">
 
-            <?php do_action('cookie-policy.before-message-row') ?>
-
             <tr>
-
-                <?php do_action('cookie-policy.before-message-col') ?>
-
                 <td colspan="<?= apply_filters('cookie-policy.message-col-colspan', 2) ?>">
                     <?= $this->message ?>
                 </td>
-
-                <?php do_action('cookie-policy.after-message-col') ?>
-
             </tr>
 
-            <?php do_action('cookie-policy.after-message-row') ?>
-
             <tr>
-
-                <?php do_action('cookie-policy.before-message-links-cols') ?>
-
                 <td width="<?= apply_filters('cookie-policy.message-more-link-width', '25%') ?>">
                     <?php if ($this->moreUrl && $this->moreLabel) : ?>
                         <a id="cookie-policy-more" href="<?= esc_url($this->moreUrl) ?>">
@@ -35,9 +19,6 @@
                         </a>
                     <?php endif ?>
                 </td>
-
-                <?php do_action('cookie-policy.before-message-close-link-col') ?>
-
                 <td>
                     <a id="cookie-policy-close"
                        href="<?= esc_url($this->closeUrl) ?>"
@@ -46,17 +27,9 @@
                         <?= esc_html($this->closeLabel) ?>
                     </a>
                 </td>
-
-                <?php do_action('cookie-policy.after-message-links-cols') ?>
-
             </tr>
 
-            <?php do_action('cookie-policy.after-links-row') ?>
-
         </table>
-
-        <?php do_action('cookie-policy.after-message-table') ?>
-
     </div>
 
     <?php do_action('cookie-policy.after-message') ?>
